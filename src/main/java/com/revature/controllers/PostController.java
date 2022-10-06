011555341bfab9be31a1ea41546bd22e226a8c32
thisonewillworkforsure <ben6228@yahoo.com>
2022-10-06 17:46:30 -0400

    trying again

--- src/main/java/com/revature/controllers/PostController.java
+++ src/main/java/com/revature/controllers/PostController.java
@@ -18,7 +18,7 @@ import com.revature.services.PostService;
 
 @RestController
 @RequestMapping("/post")
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+@CrossOrigin(origins = "https://mrbucket456.s3.amazonaws.com", allowCredentials = "true")
 public class PostController {
 
 	private final PostService postService;
