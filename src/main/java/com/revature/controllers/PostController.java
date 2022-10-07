ca307dd48bae0c05db76f9d1ac6ca93a1b47fa4e
thisonewillworkforsure <ben6228@yahoo.com>
2022-10-06 21:29:15 -0400

    testing taking away with credentials

--- src/main/java/com/revature/controllers/PostController.java
+++ src/main/java/com/revature/controllers/PostController.java
@@ -18,7 +18,7 @@ import com.revature.services.PostService;
 
 @RestController
 @RequestMapping("/post")
-@CrossOrigin(origins = "https://mrbucket456.s3.amazonaws.com", allowCredentials = "true")
+@CrossOrigin(origins = "https://mrbucket456.s3.amazonaws.com")
 public class PostController {
 
 	private final PostService postService;
